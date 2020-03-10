e77829e5cb75b9283b508045d8efe3a61e85c201
DCK-domfam <60648246+DCK-domfam@users.noreply.github.com>
2020-03-10 19:32:41 +0900

    blog backend 작업

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,27 +1,45 @@
 //server generate
-const koa = require('koa');
+const Koa = require('koa');
+const Router = require('koa-router');
 
-const app = new koa();
+const app = new Koa();
+const router = new Router();
 
-app.use((ctx,next) => {
-    console.log(ctx.url);
-    console.log(1);
-    if(ctx.query.authorized !== '1'){
-        ctx.status = 401;
-        return;
-    }
-    next();
-})
-
-app.use((ctx,next) => {    
-    console.log(2);
-    next();
-})
-
-app.use(ctx => {
-    ctx.body = 'hello world';
+//use router 
+router.get('/', ctx => {
+    ctx.body='home';
 });
 
+router.get('/about', ctx => {
+    ctx.body='Introduce';
+});
+
+
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+//use middleware
+// app.use((ctx, next) => {
+//   console.log(ctx.url);
+//   console.log(1);
+//   if (ctx.query.authorized !== '1') {
+//     ctx.status = 401;
+//     return;
+//   }
+//   next().then(() => {
+//     console.log('END');
+//   });
+// });
+
+// app.use((ctx, next) => {
+//   console.log(2);
+//   next();
+// });
+
+// app.use(ctx => {
+//   ctx.body = 'hello world';
+// });
+
 app.listen(4000, () => {
-    console.log('Listening to port 4000');
-})
+  console.log('Listening to port 4000');
+});
