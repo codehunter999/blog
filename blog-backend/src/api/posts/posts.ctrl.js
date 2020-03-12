d34bd5a1789c4f26c40afd9120490ac07a524b91
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-03-13 00:01:03 +0900

    blog backend commit

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -100,7 +100,7 @@ exports.replace = ctx => {
 };
 
 
-/* 포스트 수정(교체)
+/* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
 { title, body }
 */
@@ -108,4 +108,19 @@ exports.update = ctx => {
     //PATCH 메서드는 주어진 필드만 교체합니다.
     const { id } = ctx.params;
     //해당 id를 가진 post가 몇 번째인지 확인합니다.
-}
+    const index = posts.findIndex(p => p.id.toString() === id);
+    //포스트가 없으면 오류를 반환
+    if(index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message:'포스트가 존재하지 앖습니다.',
+        };
+        return;
+    }
+    //기존 값에 정보를 덮어 씌웁니다.
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
