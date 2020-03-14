95a96c29301f0015926624d2444c9921c92fad2f
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-03-14 18:46:42 +0900

    blog commit

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,7 +1,11 @@
 //server generate
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT } = process.env;
 
 const api = require('./api');
 
@@ -33,6 +37,12 @@ router.get('/posts', ctx => {
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+//PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+})
 
 //use middleware
 // app.use((ctx, next) => {
