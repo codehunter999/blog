148f295860ca91f8eda7da6cc67919a583050111
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-03-15 21:40:44 +0900

    원격 브랜치에 커밋

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -22,7 +22,6 @@ export const checkObjectId = (ctx, next) => {
     tags:['tag1','tag2']
   }
 */
-
 export const write = async ctx => {
   //request 검증
   const schema = Joi.object().keys({
@@ -80,7 +79,7 @@ export const list = async ctx => {
       .exec();
     const postCount = await Post.countDocuments().exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10)); //Last-Page라는 커스텀 HTTP헤더를 설정함.
-    
+     
     // !!! 200자 제한 case1 !!!!
     // ctx.body = posts
     //   .map(post => post.toJSON())
@@ -90,8 +89,6 @@ export const list = async ctx => {
     //       post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
     //   }));
 
-/* 특정 포스트 조회
-GET /api/posts/:id
     // !!! 200자 제한 case2 !!!!
     ctx.body = posts      
       .map(post => ({
@@ -105,9 +102,12 @@ GET /api/posts/:id
   }
 };
 
-/*
+
+/* 
+  특정 포스트 조회
   GET /api/posts/:id
   특정id를 가진 데이터를 조회할 때는 findById()함수를 사용
+  GET /api/posts/:id  
 */
 export const read = async ctx => {
   const { id } = ctx.params;
@@ -123,76 +123,8 @@ export const read = async ctx => {
   }
 };
 
-/* 특정 포스트 삭제
-DELETE /api/posts/:id
-*/
-exports.remove = ctx => {
-  const { id } = ctx.params;
-  //해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex(p => p.id.toString() === id);
-  //포스트가 없으면 오류를 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  //index번째 아이템을 제거합니다.
-  posts.splice(index, 1);
-  ctx.status = 204; //No Content
-};
 
-/* 포스트 수정(교체)
-PUT /api/posts/:id
-{ title, body }
-*/
-exports.replace = ctx => {
-  //PUT 메소드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
-  const { id } = ctx.params;
-  //해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex(p => p.id.toString() === id);
-  //포스트가 없으면 오류를 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  //전체 객체를 덮어 씌웁니다.
-  //따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듭니다.
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
 
-/* 포스트 수정(특정 필드 변경)
-PATCH /api/posts/:id
-{ title, body }
-*/
-exports.update = ctx => {
-  //PATCH 메서드는 주어진 필드만 교체합니다.
-  const { id } = ctx.params;
-  //해당 id를 가진 post가 몇 번째인지 확인합니다.
-  const index = posts.findIndex(p => p.id.toString() === id);
-  //포스트가 없으면 오류를 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 앖습니다.',
-    };
-    return;
-  }
-  //기존 값에 정보를 덮어 씌웁니다.
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
 /*
   데이터를 삭제할 때는 여러 종류의함수 사용
   1.remove(): 특정 조건을 만족하는 데이터를 모두 지웁니다.
