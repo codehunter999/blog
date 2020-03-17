c475046387ad3d989bbad3e234336fb28ded93c7
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-03-17 22:03:46 +0900

    20200317 저녁 커밋

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -4,7 +4,7 @@ import User from '../../models/user';
 /*
     POST /api/auth/register
     {
-        username: 'kdc_dev'
+        username: 'kdcdev'
         password: 'mypass123'
     }
 */
@@ -41,20 +41,52 @@ export const register = async ctx => {
     await user.save(); //데이터베이스에 저장
 
     //응답할 데이터에서 hashedPassword 필드 제거
-    // ㅁ CASE 1 
+    // ㅁ CASE 1
     // const data = user.toJSON();
     // delete data.hashedPassword;
     // ctx.body = data;
     // ㅁ CASE 2 : serialize함수 사용
     ctx.body = user.serialize();
-
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
+/*
+  POST /api/auth/login
+  {
+    username: 'kdcdev'
+    password: 'mypass123'
+  }
+*/
 export const login = async ctx => {
   //로그인
+  const { username, password } = ctx.request.body;
+
+  //username, password가 없으면 에러 처리
+  if(!username || !password){
+    ctx.status = 401; //unauthrized
+    return;
+  }
+
+  try{
+    const user = await User.findByUsername(username);
+    //계정이 존재하지 않으면 에러처리
+    if(!user){
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    //잘못된 비밀번호
+    if(!valid){
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  }
+  catch(e){
+    ctx.throw(500, e);
+  }
 };
 
 export const check = async ctx => {
