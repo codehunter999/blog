8d2c0f172ddd056006008da058b392a052c8a75f
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-03-29 19:56:26 +0900

    프론트 일부 수정

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -16,6 +16,23 @@ const StyledButton = styled.button`
   &:hover {
     background: ${palette.gray[6]};
   }
+  ${props =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+
+  ${props =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
 `;
 
 const Button = props => <StyledButton {...props} />;
