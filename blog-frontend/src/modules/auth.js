df5ea042b20a32f87970ace8be8f09ca7e9f68ce
codehunter999 <49275951+codehunter999@users.noreply.github.com>
2020-04-05 21:07:02 +0900

    com

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,44 +1,50 @@
-import { createAction, handleActions } from 'redux-actions';
+import {createAction, handleActions} from 'redux-actions';
 import produce from 'immer';
 
+//액션 타입 선언
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
+//액션 생성함수 정의
 export const changeField = createAction(
-  CHANGE_FIELD,
-  ({ form, key, value }) => ({
-    from, //register, login
-    key, //username, password, passwordConfirm
-    value, //실제 바꾸려는 값
-  }),
+    CHANGE_FIELD,
+    ({form, key, value}) => ({
+        form, //register, login
+        key, //username, password, passwordConfirm
+        value, //실제 바꾸려는 값
+    }),
 );
+export const initializeForm = createAction(
+    INITIALIZE_FORM,
+    form => form
+); //register/login
 
-
-export const initializeForm = createAction(INITIALIZE_FORM, form => form); //register/login
+//immutable의 Map으로 감싸기
 const initialState = {
-  register: {
-    username: '',
-    password: '',
-    passwordConfirm: '',
-  },
-  login: {
-    username: '',
-    password: '',
-  },
+    register: {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+    },
+    login: {
+        username: '',
+        password: '',
+    },
 };
 
+//리듀서 작성
 const auth = handleActions(
-  {
-    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
-      produce(state, draft => {
-        draft[form][key] = value; //예: state.register.username을 바꾼다.
-      }),
-    [INITIALIZE_FORM]: (state, { payload: form }) => ({
-      ...state,
-      [form]: initialState[form],
-    }),
-  },
-  initialState,
+    {
+        [CHANGE_FIELD]: (state, {payload: {form, key, value}}) =>
+            produce(state, draft => {
+                draft[form][key] = value; //예: state.register.username을 바꾼다.
+            }),
+        [INITIALIZE_FORM]: (state, {payload: form}) => ({
+            ...state,
+            [form]: initialState[form],
+        }),
+    },
+    initialState,
 );
 
 export default auth;
